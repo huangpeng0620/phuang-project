a4ceaad508182719ea40908f7e4d2730e7e96c1c
huangpeng0620 <phuang_0620@163.com>
2026-09-05 20:42:39 +0800

    feat: 增加相关工具类

--- phuang_common/HLock-annotation-common-starter/src/main/java/com/phuang/hlock/aspect/HLockAnnotationAspect.java
+++ phuang_common/HLock-annotation-common-starter/src/main/java/com/phuang/hlock/aspect/HLockAnnotationAspect.java
@@ -2,7 +2,7 @@ package com.phuang.hlock.aspect;
 
 import com.phuang.hlock.annotation.HLock;
 import com.phuang.hlock.handler.LockInfoHandler;
-import com.phuang.hlock.model.BusinessException;
+import com.phuang.hlock.model.HlockException;
 import com.phuang.hlock.model.LockFactory;
 import com.phuang.hlock.model.LockInfo;
 import com.phuang.hlock.model.enums.BusinessErrorEnum;
@@ -69,7 +69,7 @@ public class HLockAnnotationAspect {
             log.warn("获取分布式锁失败, method={}, lockName={}, lockType={}",
                     method.toGenericString(), lockInfo.getLockName(), hlock.lockType());
             // 直接传入错误枚举，确保异常同时保留 1001 错误码和对应提示信息。
-            throw new BusinessException(BusinessErrorEnum.REPEATSUBMIT_ERROR);
+            throw new HlockException(BusinessErrorEnum.REPEATSUBMIT_ERROR);
         }
         log.debug("获取分布式锁成功, method={}, lockName={}",
                 method.toGenericString(), lockInfo.getLockName());
