4d778676e8241af0a92c9d4016fbae0f03020cf3
huangpeng <phuang_0620@163.com>
2026-09-18 22:25:23 +0800

    feat: spring-ai-agent模块chu shi h

---
+++ phuang-ai-demo/ai-framework/spring-ai-agent/src/main/java/com/phuang/SpringAIAgentApplication.java
@@ -0,0 +1,13 @@
+package com.phuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAIAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAIAgentApplication.class, args);
+	}
+
+}
---
+++ phuang-ai-demo/ai-framework/spring-ai-agent/src/main/java/com/phuang/controller/ReactAgentController.java
@@ -0,0 +1,17 @@
+package com.phuang.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @description ReactAgentController
+ * @author huangpeng
+ * @since 2026/9/18
+ */
+@RestController
+@RequestMapping("/react")
+public class ReactAgentController {
+
+
+}
